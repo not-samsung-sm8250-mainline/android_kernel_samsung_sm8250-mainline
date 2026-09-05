a74edc4eac625012bfd67c6d4b7f5fdc00f0480a
tigelaa <tigelaa@proton.me>
2026-09-05 18:29:31 -0300

    mfd: max77705: accept PASS2 silicon revision
    Signed-off-by: tigelaa <tigelaa@proton.me>

--- drivers/mfd/max77705.c
+++ drivers/mfd/max77705.c
@@ -105,8 +105,11 @@ static int max77705_i2c_probe(struct i2c_client *i2c)
 		return -ENODEV;
 
 	pmic_rev = pmic_rev_value & MAX77705_REVISION_MASK;
-	if (pmic_rev != MAX77705_PASS3)
+	/* r8q ships PASS2; register layout matches PASS3. */
+	if (pmic_rev != MAX77705_PASS3 && pmic_rev != MAX77705_PASS2)
 		return dev_err_probe(dev, -ENODEV, "Rev.0x%x is not tested\n", pmic_rev);
+	if (pmic_rev != MAX77705_PASS3)
+		dev_info(dev, "accepting untested MAX77705 rev 0x%x\n", pmic_rev);
 
 	/* Active Discharge Enable */
 	regmap_update_bits(max77705->regmap, MAX77705_PMIC_REG_MAINCTRL1, 1, 1);
